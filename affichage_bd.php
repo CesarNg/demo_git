8388d7231d9c198b4b19961d8fbd9e75fa75f24f
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2017-05-13 00:49:19 +0200

    Update affichage_bd.php

--- affichage_bd.php
+++ affichage_bd.php
@@ -8,8 +8,8 @@
 
  
 
-$bdd= new PDO('mysql:host=127.0.0.1;dbname=absences_2_0', 'root', 'ouioui');
- //$bdd= new PDO('mysql:host=185.31.40.5;dbname=cesarng_absences_2_1', 'cesarng', 'cesar1996');
+//$bdd= new PDO('mysql:host=127.0.0.1;dbname=absences_2_0', 'root', 'ouioui');
+ $bdd= new PDO('mysql:host=185.31.40.5;dbname=cesarng_absences_2_1', 'cesarng', 'cesar1996');
 // get all products from products table
 $donnees = $bdd->query('SELECT  login, mot_de_passe FROM utilisateurs');
  while($row = $donnees->fetch(PDO::FETCH_ASSOC)) {
